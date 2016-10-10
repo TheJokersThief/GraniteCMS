f5d95e0f07928c8270ed537bc1eee4194b5dbc43
Evan Smith <esincork@gmail.com>
2016-10-10 17:38:13 +0100

    Add general site-wide helper functions

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -12,4 +12,21 @@ class SiteController extends Controller {
 		$site_name = str_replace('_', '.', $site_name);
 		return Site::where('domain', $site_name)->first()->id;
 	}
+
+	public static function getSite() {
+		// Check if request is from apache
+		if (isset($_SERVER['HTTP_HOST'])) {
+			try {
+				// Include the given site's web routes
+				return str_replace('.', '_', $_SERVER['HTTP_HOST']);
+			} catch (Exception $e) {
+				// If the site doesn't exist, return 404
+				abort(404);
+			}
+		}
+	}
+
+	public static function getSitePath() {
+		return realpath(base_path('sites/' . SiteController::getSite()));
+	}
 }
