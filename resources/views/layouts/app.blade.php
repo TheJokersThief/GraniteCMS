75365a022222507206cd1c75f2274385fe02c03b
Evan Smith <esincork@gmail.com>
2016-10-10 17:39:34 +0100

    Spruce up error reports

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,16 @@
 @include('layouts.header')
 @include('layouts.navigation')
 
+@if (count($errors) > 0)
+    <div class="alert alert-danger col-lg-offset-2 col-lg-10 col-xs-12">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 @yield('content')
 
 @include('layouts.footer')
