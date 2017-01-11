55fe5238b344e12ad0600c035b7cea1507382184
Evan Smith <esincork@gmail.com>
2017-01-11 21:43:37 +0000

    Add read-only option to text and date inputs

--- resources/views/components/date.blade.php
+++ resources/views/components/date.blade.php
@@ -1,6 +1,15 @@
-<div class="form-group {{ (isset($field['class']) ? $field['class'] : '') }}">
-	{{ \Form::label($field['name'], $field['title'], ['class' => 'control-label col-xs-12']) }}
-	<div class="col-xs-12">
-		{{ \Form::date($field['name'], $value, ['class' => 'form-control']) }}
+@if( isset($field['read_only']) && $field['read_only'] )
+	<div class="form-group {{ (isset($field['class']) ? $field['class'] : '') }}">
+		{{ \Form::label($field['name'], $field['title'], ['class' => 'control-label col-xs-12']) }}
+		<div class="col-xs-12">
+			{{ $value }}
+		</div>
 	</div>
-</div>
+@else
+	<div class="form-group {{ (isset($field['class']) ? $field['class'] : '') }}">
+		{{ \Form::label($field['name'], $field['title'], ['class' => 'control-label col-xs-12']) }}
+		<div class="col-xs-12">
+			{{ \Form::date($field['name'], $value, ['class' => 'form-control']) }}
+		</div>
+	</div>
+@endif
--- resources/views/components/text.blade.php
+++ resources/views/components/text.blade.php
@@ -1,6 +1,15 @@
-<div class="form-group {{ (isset($field['class']) ? $field['class'] : '') }}">
-	{{ \Form::label($field['name'], $field['title'], ['class' => 'control-label col-xs-12']) }}
-	<div class="col-xs-12">
-		{{ \Form::text($field['name'], $value, ['class' => 'col-sm-12 col-xs-12', 'placeholder' => (isset($field['placeholder']) ? $field['placeholder'] : '') ]) }}
+@if(isset($field['read_only']) && $field['read_only'])
+	<div class="form-group {{ (isset($field['class']) ? $field['class'] : '') }}">
+		{{ \Form::label($field['name'], $field['title'], ['class' => 'control-label col-xs-12']) }}
+		<div class="col-xs-12">
+			{{ $value }}
+		</div>
 	</div>
-</div>
+@else
+	<div class="form-group {{ (isset($field['class']) ? $field['class'] : '') }}">
+		{{ \Form::label($field['name'], $field['title'], ['class' => 'control-label col-xs-12']) }}
+		<div class="col-xs-12">
+			{{ \Form::text($field['name'], $value, ['class' => 'col-sm-12 col-xs-12', 'placeholder' => (isset($field['placeholder']) ? $field['placeholder'] : '') ]) }}
+		</div>
+	</div>
+@endif
