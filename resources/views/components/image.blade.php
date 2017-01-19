84383de183c91a4ea055cb08b2789ec3df0ba8b1
Evan Smith <esincork@gmail.com>
2017-01-19 14:56:06 +0000

    Change image component to use Form class

--- resources/views/components/image.blade.php
+++ resources/views/components/image.blade.php
@@ -1,8 +1,7 @@
 <div class="form-group {{ (isset($field['class']) ? $field['class'] : '') }}">
 	{{ \Form::label($field['name'], $field['title'], ['class' => 'control-label col-xs-12']) }}
 	<div class="col-xs-12">
-		<input type="file" name="{{ $field['name'] }}" />
-
+		{{ Form::file($field['name']) }}
 		@if( isset($value) && $value != null )
 			<img src="{{ url($value) }}" style="max-height: 400px">
 		@endif
