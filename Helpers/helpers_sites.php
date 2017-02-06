17dae5901bf86eb86e30810ac9bdaf7fa0b3680d
Evan Smith <esincork@gmail.com>
2017-02-06 19:12:12 +0100

    Create helper to correctly parse the site asset URL

---
+++ Helpers/helpers_sites.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Determine URL to individual site asset
+ *     Rewrites a standard URL form to match our asset URL structure
+ * @param  string $url
+ * @return string
+ */
+function siteAsset($url)
+{
+    preg_match('/(.+)\/(.+)?\/?(.+)?\/?(.+)?\/?/', $url, $matches);
+
+    if (isset($matches[1])) {
+        $file = null;
+        $subfolder1 = null;
+        $subfolder2 = null;
+        $subfolder3 = null;
+
+        if (isset($matches[4])) {
+            // 3 subfolders
+            $subfolder3 = $matches[3];
+            $subfolder2 = $matches[2];
+            $subfolder1 = $matches[1];
+            $file = $matches[4];
+        } else if (isset($matches[3])) {
+            // 2 subfolders
+            $subfolder2 = $matches[2];
+            $subfolder1 = $matches[1];
+            $file = $matches[3];
+        } else if (isset($matches[2])) {
+            // 1 subfolder
+            $subfolder1 = $matches[1];
+            $file = $matches[2];
+        } else {
+            // no subfolders
+            $file = $matches[1];
+        }
+
+        return route('site-asset', [$file, $subfolder1, $subfolder2, $subfolder3]);
+    }
+}
