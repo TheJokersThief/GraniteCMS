5a02ca0b7a14b26fcc77525543cfd921cd846eef
Evan Smith <esincork@gmail.com>
2017-03-02 13:50:05 +0000

    Add comments to SiteController

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -10,12 +10,25 @@ use App\Site;
 
 class SiteController extends Controller
 {
+    /**
+     * Return numeric site ID for given domain name
+     * @param  string $site_name Domain name
+     * @return int
+     */
     public static function getSiteID($site_name)
     {
         $site_name = str_replace('_', '.', $site_name);
         return Site::where('domain', $site_name)->first()->id;
     }
 
+    /**
+     * Get the current site's friendly domain name
+     *
+     * If it's executed in the kernel, the DB isn't ready yet
+     * so we can't check against the alias unfortunately.
+     * @param  boolean $kernel Whether the function is executed in the kernel or not
+     * @return string
+     */
     public static function getSite($kernel = false)
     {
         // Check if request is from apache
@@ -35,11 +48,22 @@ class SiteController extends Controller
         }
     }
 
+    /**
+     * Determines the absolute path in the system to the current site
+     * @return string
+     */
     public static function getSitePath()
     {
         return realpath(base_path('sites/' . SiteController::getSite()));
     }
 
+    /**
+     * Determines the absolute path to the .env file that should be used.
+     *
+     * This allows individual sites to use their own .env files
+     * @param  string $site
+     * @return string
+     */
     public static function getEnvPath($site = null)
     {
         $site = ($site == null) ? self::getSite(true) : $site;
@@ -51,6 +75,11 @@ class SiteController extends Controller
         }
     }
 
+    /**
+     * During command-line execution, determine which site to use by
+     * using a --site flag
+     * @return string
+     */
     public static function getSiteArg()
     {
         $args = $GLOBALS['argv'];
