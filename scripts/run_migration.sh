9d2d3a688d58b5f03ba3dbe34c6d39f8479b3a65
Evan Smith <esincork@gmail.com>
2017-05-14 18:34:51 +0100

    Use recursive migrations for running

--- scripts/run_migration.sh
+++ scripts/run_migration.sh
@@ -19,14 +19,4 @@ SITE=$1
 ACTION=$2
 shift 2
 
-if [[ ! -z "$ACTION" ]] && [ ":refresh" == $ACTION ]; then
-	# If action not empty and the action is refresh
-
-	# Run a reset of the site's migrations and then 
-	# run the migrations again. Simulates a site-specific
-	# migration refresh
-	php ../artisan migrate:reset --path="sites/${SITE}/theme/migrations/" -vv --site=$SITE
-	php ../artisan migrate --path="sites/${SITE}/theme/migrations/" -vv --site=$SITE
-else
-	php ../artisan migrate$2 --path="sites/${SITE}/theme/migrations/" -vv --site=$SITE
-fi
+php ../artisan migrate$2 --recrusive --path="sites/${SITE}/theme/migrations/" -vv --site=$SITE
