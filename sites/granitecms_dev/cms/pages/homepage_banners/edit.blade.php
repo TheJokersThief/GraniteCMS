d6c7e8de49661a83ac94f62581c2e05719af087f
Evan Smith <esincork@gmail.com>
2017-05-20 10:34:08 +0100

    Cleanup default site

---
+++ sites/granitecms_dev/cms/pages/homepage_banners/edit.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="right_col" role="main">
+   <div class="col-md-12 col-sm-12 col-xs-12">
+		{!! $form !!}
+   </div>
+</div>
+<!-- /page content -->
+
+@stop
---
+++ sites/granitecms_dev/theme/Install.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Sites\granitecms_dev\theme;
+
+use App\Core\Capabilities;
+use App\Http\Controllers\SiteController;
+use App\MenuItem;
+
+/**
+ * /------------------------------------------------------
+ * |
+ * |    The Install class is loaded when the site is
+ * |    first requested and performs any action
+ * |    written in the install() method
+ * |
+ */
+
+class Install
+{
+    /**
+     * Installation steps to activate theme functionality
+     */
+    public function install()
+    {
+        $siteID = SiteController::getSiteID(SiteController::getSite());
+
+        $cmsMenu = getMenuByName("CMS Menu");
+        Capabilities::addNewCapability('homepage_banners', 4, 4, 4, 2);
+
+        $sites = MenuItem::create(['name' => 'Sites',
+            'link' => '#!',
+            'parent' => $cmsMenu->id,
+            'site' => $siteID,
+            'page' => "homepage_banners",
+        ]);
+
+        MenuItem::create(['name' => 'All Sites',
+            'link' => '/cms/homepage_banners',
+            'parent' => $sites->id,
+            'site' => $siteID,
+            'page' => "homepage_banners",
+        ]);
+
+        MenuItem::create(['name' => 'Add Site',
+            'link' => '/cms/homepage_banners/create',
+            'parent' => $sites->id,
+            'site' => $siteID,
+            'page' => "homepage_banners",
+        ]);
+
+    }
+}
