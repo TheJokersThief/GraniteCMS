00acd1de9f7cd9e3b85bdb6a86ca896ed2496a1d
Evan Smith <esincork@gmail.com>
2017-06-06 20:52:33 +0100

    Enable AWS DB config

--- config/database.php
+++ config/database.php
@@ -1,5 +1,20 @@
 <?php
 
+
+if( isset($_SERVER['RDS_HOSTNAME']) ){
+    if (!defined('RDS_HOSTNAME')) {
+      define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
+      define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
+      define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
+      define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
+    }
+} else{
+    define('RDS_HOSTNAME', env('DB_HOST', 'localhost') );
+    define('RDS_USERNAME', env('DB_USERNAME', 'forge') );
+    define('RDS_PASSWORD', env('DB_PASSWORD', 'forge') );
+    define('RDS_DB_NAME', env('DB_DATABASE', '') );
+}
+
 return [
 
     /*
@@ -54,11 +69,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host'      => RDS_HOSTNAME,
+            'database'  => RDS_DB_NAME,
+            'username'  => RDS_USERNAME,
+            'password'  => RDS_PASSWORD,
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
